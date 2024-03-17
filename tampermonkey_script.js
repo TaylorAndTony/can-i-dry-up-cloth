8160ba9f0c9f8def1fc8f0e531ce1ec54dd483e2
TaylorAndTony <doyijian@163.com>
2024-03-17 08:51:16 +0800

    fix wind speed css selector mathes too many chars

--- tampermonkey_script.js
+++ tampermonkey_script.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         能晾衣服吗信息获取脚本
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  try to take over the world!
-// @author       You
+// @version      0.2
+// @description  与 can-i-dry-up-cloth 前端共同工作，提供跨域访问 nmc.cn 的数据
+// @author       TaylorAndTony
 // @match        http://127.0.0.1:5500/index.html
 // @match        https://taylorandtony.github.io/can-i-dry-up-cloth/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=nmc.cn
@@ -12,12 +12,16 @@
 // @grant        GM_xmlhttpRequest
 // ==/UserScript==
 
-// by default, jQuery and SweetAlert2 are loaded up there ⬆⬆️
 this.$ = this.jQuery = unsafeWindow.$ || unsafeWindow.jQuery || jQuery.noConflict(true);
 
+// 配置项
+// 目标网址
 const URL = 'http://www.nmc.cn/publish/forecast/ASD/yantai.html'
-const WIND = '#day0 > div.hour3.hbg > div:nth-child(5)'
+// 风速 css 选择器
+const WIND = '#day0 > div:nth-child(1) > div:nth-child(5)'
+// 天气（晴、小雨等）css 选择器
 const WEATHER = '#day7 > div.weather.pull-left.selected > div > div:nth-child(9)';
+// 风速正则表达式
 const WIND_REGEX = /(\d+.\d+)m\/s/;
 
 async function __Get_Data() {
